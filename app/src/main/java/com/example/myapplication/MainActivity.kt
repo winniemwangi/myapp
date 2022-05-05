e6f1cb6a84ba13027668e20d22f9f9f214e2e541
winniemwangi <winniewameremwangi@gmail.com>
2022-05-05 20:20:47 +0300

    add my application

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnBmi: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnBmi = findViewById(R.id.btnBmi)
+        btnBmi.setOnClickListener {
+            val intent = Intent(this, BMICalculatorActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
