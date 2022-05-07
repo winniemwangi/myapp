77a50fcc66d2236f4c8be200b3856723d7a33294
winniemwangi <winniewameremwangi@gmail.com>
2022-05-07 11:02:53 +0300

    add the send money

--- app/src/main/java/com/example/myapplication/sendmoney.kt
+++ app/src/main/java/com/example/myapplication/sendmoney.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class BMICalculatorActivity : AppCompatActivity() {
-    lateinit var btnSend: Button
+class sendmoney: AppCompatActivity() {
+    lateinit var btnCancel: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bmicalculator)
-        btnSend = findViewById(R.id.btnSend)
-        btnSend.setOnClickListener {
-            val intent = Intent(this, sendmoney::class.java)
+        setContentView(R.layout.activity_sendmoney)
+        btnCancel = findViewById(R.id.btnCancel)
+        btnCancel.setOnClickListener {
+            val intent = Intent(this, BMICalculatorActivity::class.java)
             startActivity(intent)
         }
     }
